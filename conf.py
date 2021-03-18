8c78012e9ca73e7f6dc2408c7e179d41d4178322
Andrei Dmitriev <dmitriev2.ps4@gmail.com>
2021-03-18 23:32:05 +0100

    Updated to look better.

--- conf.py
+++ conf.py
@@ -14,6 +14,14 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
+extensions = [
+    'myst_parser',
+    "sphinx_rtd_theme"
+]
+
+html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
