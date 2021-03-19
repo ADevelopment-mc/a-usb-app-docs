9bc909bb6d405cba3be78fce2ac47ab98488c9b6
Andrei Dmitriev <dmitriev2.ps4@gmail.com>
2021-03-19 10:12:51 +0100

    l

--- docs/conf.py
+++ docs/conf.py
@@ -14,14 +14,20 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 extensions = [
-    'myst_parser',
-    "sphinx_rtd_theme"
+    'myst_parser'
 ]
-
-html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
